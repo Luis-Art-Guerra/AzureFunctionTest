cf10000c53a4e33d1433f5fb16f07c28b47f7d03
Guerra Garcia, Luis Arturo <LUISGUE@iadb.org>
2021-11-03 18:01:25 -0400

    Add project files.

---
+++ FunctionAppTestException2/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionAppTestException2
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "testFunctionException2")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            RequestModel data = JsonConvert.DeserializeObject<RequestModel>(requestBody);
+
+            string responseMessage = $"Data received: {data.MyProp}";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ FunctionAppTestException2/RequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionAppTestException2
+{
+    public enum TestEnum
+    {
+        Generic,
+        MifFinancial
+    }
+    public class RequestModel
+    {
+        public TestEnum MyProp { get; set; }
+    }
+}
